4d398c89ad141dab4e328147d332fa5fed2d0faf
DESKTOP-VM5DBLN\SNG-PC-TIEN <tiennnc3@gmail.com>
2022-09-29 09:16:01 +0700

    76

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,27 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
-
+  const [isVaild, setIsVaild] = useState(true);
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setIsVaild(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsVaild(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
-  };
-
+    };
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input type="text" onChange={goalInputChangeHandler} style = {{background: !isVaild? "red": "#ccc"}} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
